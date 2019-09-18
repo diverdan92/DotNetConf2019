9f28b8a89757dd92573ee44e716ea13f321caed9
Daniel Jacobson <dajaco@microsoft.com>
2019-09-18 14:52:12 -0700

    Making the demo a calculator app based on WPF Samples

---
+++ DotNetConf/MyTextBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DotNetConf
+{
+	internal sealed class MyTextBox : TextBox
+	{
+		protected override void OnPreviewGotKeyboardFocus(KeyboardFocusChangedEventArgs e)
+		{
+			e.Handled = true;
+			base.OnPreviewGotKeyboardFocus(e);
+		}
+	}
+}
