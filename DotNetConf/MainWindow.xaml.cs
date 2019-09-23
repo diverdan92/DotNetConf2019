53183a8f36c10c1422442c0f72b9ab25b5cb1dcd
Daniel Jacobson <dajaco@microsoft.com>
2019-09-23 12:11:15 -0700

    Adding App Center!

--- DotNetConf/MainWindow.xaml.cs
+++ DotNetConf/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace DotNetConf
 		public MainWindow()
 		{
 			InitializeComponent();
+			AppCenter.Start("4d9b1326-3c35-4d43-97e0-0ab22c10ec98",
+				   typeof(Analytics), typeof(Crashes));
 			_paper = new PaperTrail(this);
 			ProcessKey('0');
 			EraseDisplay = true;
